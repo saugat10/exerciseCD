0f06c7feab1026fc44de92b28fa769c218941192
saugat10 <saugatkhatiwada10@gmail.com>
2022-10-27 10:06:18 +0200

    cow class and test cases

---
+++ exerciseCD/Models/Cow.cs
@@ -0,0 +1,30 @@
+﻿namespace exerciseCD.Models
+{
+    public class Cow
+    {
+        public int myProperty { get; set; }
+
+        public string Name { get; set; }
+
+        public int Weight { get; set; }
+
+        public void ValidateName()
+        {
+            if (Name == null)
+            {
+                throw new ArgumentNullException("Name cannot be null");
+            }
+
+            if(Name.Length < 2) 
+            {
+                throw new ArgumentException("Name must have atleast 3 characters");
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Cow: {Name} weights {Weight} pounds";
+        }
+
+    }
+}
---
+++ exerciseCDTests/Models/CowTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using exerciseCD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exerciseCD.Models.Tests
+{
+    [TestClass()]
+    public class CowTests
+    {
+        [TestMethod()]
+        public void ValidateNameTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
